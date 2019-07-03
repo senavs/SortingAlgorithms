bc3d7c675c1d0994b9c62e50814329708838fe83
Matheus Sena <sena.matheus14@gmail.com>
2019-07-03 09:35:26 -0300

    Add inplace parameter

--- sort/algorithms.py
+++ sort/algorithms.py
@@ -1,7 +1,12 @@
 from ..utils.functions import swap
 
-def selection(array):
+def selection(array, inplace=False):
+	if not inplace:
+		cp_array = array.copy()
+		return selection(cp_array, inplace=True)
+
 	for pivot in range(len(array)):
 		for index in range(pivot, len(array)):
 			if array[index] < array[pivot]:
 				swap(array, pivot, index)
+	return array
