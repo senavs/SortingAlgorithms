7e1e9dfc17171e5ec0d309689360f847fab5ef3a
Matheus Sena <sena.matheus14@gmail.com>
2019-07-03 10:10:25 -0300

    Create bubble Sort

--- sort/algorithms.py
+++ sort/algorithms.py
@@ -1,6 +1,6 @@
 from ..utils.functions import swap
 
-def selection(array, inplace=False):
+def selection(array, inplace=True):
 	if not inplace:
 		cp_array = array.copy()
 		return selection(cp_array, inplace=True)
@@ -9,4 +9,17 @@ def selection(array, inplace=False):
 		for index in range(pivot, len(array)):
 			if array[index] < array[pivot]:
 				swap(array, pivot, index)
+	return array
+
+def bubble(array, inplace=True):
+	if not inplace:
+		cp_array = array.copy()
+		return bubble(cp_array, inplace=True)
+
+	if len(array) <= 1:
+		return array
+	for limit in range(len(array)):
+		for index in range(1, len(array)-limit):
+			if array[index] < array[index-1]:
+				swap(array, index, index-1)
 	return array
