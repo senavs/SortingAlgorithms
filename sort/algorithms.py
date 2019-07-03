3fcee424afa5d02106e2242b587b8219c3a514a8
Matheus Sena <sena.matheus14@gmail.com>
2019-07-03 10:21:18 -0300

    Create insertion sort

--- sort/algorithms.py
+++ sort/algorithms.py
@@ -1,25 +1,41 @@
 from ..utils.functions import swap
 
 def selection(array, inplace=True):
-	if not inplace:
-		cp_array = array.copy()
-		return selection(cp_array, inplace=True)
+    if not inplace:
+        cp_array = array.copy()
+        return selection(cp_array, inplace=True)
 
-	for pivot in range(len(array)):
-		for index in range(pivot, len(array)):
-			if array[index] < array[pivot]:
-				swap(array, pivot, index)
-	return array
+    for pivot in range(len(array)):
+        for index in range(pivot, len(array)):
+            if array[index] < array[pivot]:
+                swap(array, pivot, index)
+    return array
 
 def bubble(array, inplace=True):
-	if not inplace:
-		cp_array = array.copy()
-		return bubble(cp_array, inplace=True)
+    if not inplace:
+        cp_array = array.copy()
+        return bubble(cp_array, inplace=True)
 
-	if len(array) <= 1:
-		return array
-	for limit in range(len(array)):
-		for index in range(1, len(array)-limit):
-			if array[index] < array[index-1]:
-				swap(array, index, index-1)
-	return array
+    if len(array) <= 1:
+        return array
+    for limit in range(len(array)):
+        for index in range(1, len(array)-limit):
+            if array[index] < array[index-1]:
+                swap(array, index, index-1)
+    return array
+
+def insertion(array, inplace=True):
+    if not inplace:
+        cp_array = array.copy()
+        return insertion(cp_array, inplace=True)
+
+    if len(array) <= 1:
+        return array
+    for pivot in range(1, len(array)):
+        for index in range(len(array[:pivot])-1, -1, -1):
+            if array[pivot] < array[index]:
+                swap(array, pivot, index)
+                pivot = index
+            else:
+                break
+    return array
