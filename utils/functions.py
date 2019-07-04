210818820b769a0c8b60f0960376fcf34e4ababf
Matheus Sena <sena.matheus14@gmail.com>
2019-07-04 09:54:34 -0300

    Delete partition function and add to quicksort function

--- utils/functions.py
+++ utils/functions.py
@@ -3,14 +3,3 @@ def swap(array, pos1, pos2):
 	tmp = array[pos1]
 	array[pos1] = array[pos2]
 	array[pos2] = tmp
-
-# Partition function for quicksort algorithm
-def partition(array, start, end):
-    pivot_index = start
-    pivot_value = array[end]
-    for i in range(start, end):
-        if array[i] < pivot_value:
-            swap(array, i, pivot_index)
-            pivot_index += 1
-    swap(array, pivot_index, end)
-    return pivot_index
