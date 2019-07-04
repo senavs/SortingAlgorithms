867dd3e02144ed15bc6ad9879e84048d35b557fa
Matheus Sena <sena.matheus14@gmail.com>
2019-07-04 09:37:09 -0300

    Add function descriptions

--- utils/functions.py
+++ utils/functions.py
@@ -1,8 +1,10 @@
+# Swap two elements in an array
 def swap(array, pos1, pos2):
 	tmp = array[pos1]
 	array[pos1] = array[pos2]
 	array[pos2] = tmp
 
+# Partition function for quicksort algorithm
 def partition(array, start, end):
     pivot_index = start
     pivot_value = array[end]
