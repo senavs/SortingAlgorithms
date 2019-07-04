943a362031c6614b49648b14240235ee71d5452c
Matheus Sena <sena.matheus14@gmail.com>
2019-07-04 09:36:00 -0300

    Create partition function for QuickSort

--- utils/functions.py
+++ utils/functions.py
@@ -2,3 +2,13 @@ def swap(array, pos1, pos2):
 	tmp = array[pos1]
 	array[pos1] = array[pos2]
 	array[pos2] = tmp
+
+def partition(array, start, end):
+    pivot_index = start
+    pivot_value = array[end]
+    for i in range(start, end):
+        if array[i] < pivot_value:
+            swap(array, i, pivot_index)
+            pivot_index += 1
+    swap(array, pivot_index, end)
+    return pivot_index
